9bc43c0170d16253983e557b3ed2b10ee71f2dc6
YuLi <ephemerablue@gmail.com>
2017-08-06 00:45:18 +0800

    no message

--- estore/admin.py
+++ estore/admin.py
@@ -3,3 +3,8 @@ from .models import Product
 
 # Register your models here.
 admin.site.register(Product)
+
+auth | user | Can change user
+estore | product | Can add product
+estore | product | Can change product
+estore | product | Can delete product
--- estore/urls.py
+++ estore/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
-    url(r'^create$', views.product_create, name='product_create'),
+    url(r'^$', views.ProductList.as_view(), name='product_list'),
+    url(r'^dashboard/users/$', views.UserList.as_view(), name='dashboard_user_list'),
+    url(r'^dashboard/users/(?P<pk>\d+)/addtostaff$', views.UserAddToStaff.as_view(), name='dashboard_user_addtostaff'),
+    url(r'^dashboard/users/(?P<pk>\d+)/removefromstaff$', views.UserRemoveFromStaff.as_view(), name='dashboard_user_removefromstaff'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
