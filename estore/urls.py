7fd89aaa1e06b18097dd8f45f7e68c96e350513d
YuLi <ephemerablue@gmail.com>
2017-08-09 21:18:24 +0800

    no message

--- estore/urls.py
+++ estore/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/addtocart$', views.ProductAddToCart.as_view(), name='product_addtocart'),
 
     url(r'^cart/$', views.CartDetailFromRequest.as_view(), name='cart_detail'),
+    url(r'^cart/checkout$', views.OrderCreateCartCheckout.as_view(), name='cart_checkout'),
 
     url(r'^dashboard/products/$', views.ProductList.as_view(template_name='estore/dashboard_product_list.html', permission_required='estore.change_product'), name='dashboard_product_list'),
     url(r'^dashboard/products/create$', views.ProductCreate.as_view(), name='dashboard_product_create'),
@@ -18,6 +19,8 @@ urlpatterns = [
     url(r'^dashboard/users/$', views.UserList.as_view(), name='dashboard_user_list'),
     url(r'^dashboard/users/(?P<pk>\d+)/addtostaff$', views.UserAddToStaff.as_view(), name='dashboard_user_addtostaff'),
     url(r'^dashboard/users/(?P<pk>\d+)/removefromstaff$', views.UserRemoveFromStaff.as_view(), name='dashboard_user_removefromstaff'),
+
+    url(r'^order/(?P<token>[0-9a-f-]+)/$', views.OrderDetail.as_view(), name='order_detail'),
 ]
 
 if settings.DEBUG:
